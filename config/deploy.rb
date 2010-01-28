fed7fe7407d32200d02d98b89621e0d4a91b9078
Visnu Pitiyanuvath <visnupx@gmail.com>
2010-01-28 11:48:27 -0800

    capify

---
+++ config/deploy.rb
@@ -0,0 +1,18 @@
+set :application, "theglamourist"
+set :scm, "git"
+set :repository, "git@github.com:visnup/theglamourist.git"
+set :deploy_via, :remote_cache
+
+set :user, "app"
+set :rails_env, "production"
+set :use_sudo, false
+
+role :web, "theglamourist.com"
+role :db, "theglamourist.com", :primary => true
+
+namespace :deploy do
+  desc "Restart Application"
+  task :restart, :roles => :web do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
