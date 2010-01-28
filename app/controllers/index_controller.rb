ef4862a2cd333ce9dff7b54b2d625ae57ee0dfdf
Visnu Pitiyanuvath <visnupx@gmail.com>
2010-01-28 06:07:28 -0800

    portfolio page based on flickr

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,2 +1,15 @@
+require 'open-uri'
+
 class IndexController < ApplicationController
+  def portfolio
+    url = 'http://www.flickr.com/photos/20451842@N05/sets/72157617757170924/'
+    @photos = open url do |f|
+      n = Nokogiri::HTML f
+      n.search('#setThumbs span.photo_container').map do |s|
+        { :id => s['id'],
+          :href => s.at('a.image_link')['href'],
+          :src => s.at('img')['src'] }
+      end
+    end
+  end
 end
