ef8d45aa690d7b91472ed9e9cf2f53839879d1b3
Visnu Pitiyanuvath <visnupx@gmail.com>
2010-01-28 23:09:24 -0800

    more top nav changes for better stability. affects #1.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,19 +1,32 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function() {
+  var expand = function() {
+    $(this)
+      .animate({ width: $(this).data('selectedWidth') }, 'fast')
+      .parent().siblings().find('a')
+        .each(function() {
+          $(this)
+            .clearQueue()
+            .animate({ width: $(this).data('startWidth') }, 'fast');
+        });
+  };
+
   // setup the top nav
-  $('#nav li a[href=' + window.location.pathname + ']')
-    .addClass('selected')
-    .css('width', function() { return $(this).width() + 50; });
-  $('#nav li a:not(.selected)').hover(
-    function() {
-      if (!$(this).data('originalWidth'))
-        $(this).data('originalWidth', $(this).width());
-      $(this).animate({ width: $(this).width() + 50 }, 'fast');
-    },
-    function() {
-      $(this).animate({ width: $(this).data('originalWidth') }, 'fast');
-    });
+  $('#nav li a')
+    .each(function() {
+      var w = $(this).width();
+      $(this).data({ 'startWidth': w, 'selectedWidth': w + 50 });
+    })
+    .mouseenter(expand)
+    .click(function() {
+      $('#nav li a.selected').removeClass('selected');
+      $(this).addClass('selected');
+    })
+    .filter('a[href=' + window.location.pathname + ']')
+      .addClass('selected')
+      .css('width', function() { return $(this).data('selectedWidth'); });
+  $('#nav').mouseleave(function() { expand.apply($('#nav li a.selected')); });
 
   // focus first form elements
   $('#page form :text:first').focus();
