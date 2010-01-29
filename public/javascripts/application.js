749d1e7cae4b13778891d1ec80f167cbfd98978f
Visnu Pitiyanuvath <visnupx@gmail.com>
2010-01-28 16:48:04 -0800

    js housekeeping

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,20 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-$(document).ready(function() { $('#page form :text:first').focus(); });
+$(document).ready(function() {
+  // setup the top nav
+  $('#nav li a[href=' + window.location.pathname + ']')
+    .addClass('selected')
+    .css('width', function() { return $(this).width() + 50; });
+  $('#nav li a:not(.selected)').hover(
+    function() {
+      if (!$(this).data('originalWidth'))
+        $(this).data('originalWidth', $(this).width());
+      $(this).animate({ width: $(this).width() + 50 });
+    },
+    function() {
+      $(this).animate({ width: $(this).data('originalWidth') }, 'fast');
+    });
+
+  // focus first form elements
+  $('#page form :text:first').focus();
+});
