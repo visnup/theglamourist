bb6edd796db8f2c1a7992aaa57628fe7a9393e54
Visnu Pitiyanuvath <visnupx@gmail.com>
2010-08-07 11:24:00 -0700

    new day new bundler

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,10 +18,21 @@ namespace :deploy do
 end
 
 namespace :bundle do
+  task :default do
+    symlink
+    install
+  end
+
+  desc "Symlink the bundle install location to a shared location"
+  task :symlink do
+    run "[ -d #{shared_path}/bundle ] || mkdir #{shared_path}/bundle"
+    run "ln -fs #{shared_path}/bundle #{release_path}/vendor/bundle"
+  end
+
   desc "Check gem dependencies"
   task :install do
-    run "cd #{release_path} && bundle install"
+    run "cd #{release_path} && bundle install --deployment"
   end
 end
 
-after "deploy:update_code", "bundle:install"
+after "deploy:update_code", "bundle"
