8da82f89bef8c92736c4eb5480f7c8a6062717c0
Visnu Pitiyanuvath <visnupx@gmail.com>
2012-03-24 15:13:51 -0700

    restyle contact form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def asset_data_uri(path)
+    asset  = asset_paths.asset_for path, nil
+    base64 = Base64.encode64(asset.to_s).gsub(/\s+/, "")
+    "data:#{asset.content_type};base64,#{Rack::Utils.escape(base64)}"
+  end
 end
