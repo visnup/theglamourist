53826f808afe862ea19a829c377f740c6b6e8c49
Visnu Pitiyanuvath <visnupx@gmail.com>
2012-04-05 21:40:26 -0700

    home page wireframe

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,7 +1,7 @@
 require 'open-uri'
 
 class IndexController < ApplicationController
-  before_filter :fetch_albums, only: [:index, :portfolio]
+  before_filter :fetch_albums, :fetch_instagram, only: [:index, :portfolio]
   caches_page :index, :portfolio
 
   def login
@@ -40,5 +40,13 @@ class IndexController < ApplicationController
           end
         end
       end
+    end
+
+    def fetch_instagram
+      @instagram =
+        Rails.cache.fetch 'instagram' do
+          url = 'https://api.instagram.com/v1/users/538328/media/recent/?access_token=538328.643541b.be39dd953e644df58d2ce0f2460b049c'
+          open url do |f| JSON.parse(f.read)['data'] end
+        end
     end
 end
