44a4197bb87a9c61eb81b4071cebf09c6fe940bc
Visnu Pitiyanuvath <visnupx@gmail.com>
2013-06-22 16:57:09 -0700

    add more images to blog aside

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -34,34 +34,6 @@ class IndexController < ApplicationController
   end
 
   private
-    def graph_url path; "https://graph.facebook.com/#{path}" end
-
-    def fetch_albums
-      @albums = Rails.cache.fetch 'facebook' do
-        open graph_url('theglamourist/albums') do |f|
-          JSON.parse(f.read)['data'].select do |album|
-            album['type'] == 'normal' && album['name'] != 'Cover Photos'
-          end.each do |album|
-            cover = album['cover_photo'] =
-              open graph_url("#{album['cover_photo']}") do |f|
-                JSON.parse f.read
-              end
-
-            next unless cover
-            cover['class'] = 'cover ' + (cover['width'] > cover['height'] ? 'landscape' : 'portrait')
-            open "http://saturated.theglamourist.com/?url=#{CGI.escape cover['picture']}" do |saturated|
-              hsla = JSON.parse saturated.read
-              cover['saturated'] = "hsla(#{hsla['h']}, #{hsla['s']}%, #{hsla['l']}%, #{hsla['a']})"
-            end
-          end.select do |album|
-            album['cover_photo']
-          end.sort_by do |album|
-            [ 'Weddings', 'Before & Afters', 'Fashion', 'Engagement Sessions',
-              'Boudoir', 'Press' ].index(album['name']) || 1000
-          end
-        end
-      end
-    end
 
     def fetch_profile
       @photo =
