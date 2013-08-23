1ab3c016a2c4cb5d0413ec67369da1f40a911959
Visnu Pitiyanuvath <visnupx@gmail.com>
2013-08-22 23:13:33 -0700

    vcr tape for blog spec

--- spec/features/blog_spec.rb
+++ spec/features/blog_spec.rb
@@ -4,13 +4,15 @@ describe 'reading the blog' do
   let!(:post) { FactoryGirl.create :post }
 
   it 'should show me blog entries' do
-    visit '/'
-    expect(page).to have_content(post.description)
+    VCR.use_cassette 'should show me the blog entries' do
+      visit '/'
+      expect(page).to have_content(post.description)
 
-    click_on 'Blog'
-    expect(page).to have_content(post.html)
+      click_on 'Blog'
+      expect(page).to have_content(post.html)
 
-    click_on post.title
-    expect(page).to have_content(post.title)
+      click_on post.title
+      expect(page).to have_content(post.title)
+    end
   end
 end
