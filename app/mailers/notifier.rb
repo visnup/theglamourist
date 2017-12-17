ad844447ba2be25d6bf67d045d6270928e6e73c5
Visnu Pitiyanuvath <visnup@gmail.com>
2017-12-17 14:17:50 -0800

    don't try to send email on behalf of

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,11 +1,10 @@
 class Notifier < ActionMailer::Base
   default to:      'jane@theglamourist.com',
-          cc:      'vi@theglamourist.com',
           subject: '[theglamourist] New Inquiry'
 
   def new_contact(contact)
     @contact = contact
-    mail from:    "#{@contact.name} <#{@contact.email}>",
+    mail from:    "#{@contact.name} <inquiry@theglamourist.com>",
          subject: "[theglamourist] #{@contact.event_at.to_date.to_s(:long)}"
   end
 end
