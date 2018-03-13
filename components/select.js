43d7ea1e0106d2cc6b2512669690c2946067dba4
Visnu Pitiyanuvath <visnup@gmail.com>
2018-03-12 21:23:45 -0700

    svg background!

--- components/select.js
+++ components/select.js
@@ -2,4 +2,6 @@ import Input from './input'
 
 export default Input.withComponent('select').extend`
   appearance: none;
+  background: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg"><path d="M0,4L6,4L3,8Z" /></svg>') no-repeat 100% 50%;
+  background-size: 12px 12px;
 `
