854ce7806412dd0372706cdda2a30fe5e0262e6b
Visnu Pitiyanuvath <visnup@gmail.com>
2018-03-12 20:52:07 -0700

    styled inputs

---
+++ components/input.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export default styled.input`
+  padding: 10px;
+  border: solid 1px lightgray;
+  border-radius: 3px;
+`
--- sections/contact.js
+++ sections/contact.js
@@ -1,4 +1,7 @@
 import Section from '../components/section'
+import Input from '../components/input'
+import Select from '../components/select'
+import Textarea from '../components/textarea'
 
 const Contact = () => (
   <Section>
@@ -7,42 +10,42 @@ const Contact = () => (
       <p>
         <label>
           Name<br/>
-          <input type="text" aria-label="Name" name="entry.1437799755" required />
+          <Input type="text" aria-label="Name" name="entry.1437799755" required />
         </label>
       </p>
 
       <p>
         <label>
           Email address<br/>
-          <input type="email" autocomplete="email" aria-label="Your email" name="emailAddress" required />
+          <Input type="email" autocomplete="email" aria-label="Your email" name="emailAddress" required />
         </label>
       </p>
 
       <p>
         <label>
           Phone number<br/>
-          <input type="text" aria-label="Phone number" name="entry.576562644" required />
+          <Input type="text" aria-label="Phone number" name="entry.576562644" required />
         </label>
       </p>
 
       <p>
         <label>
           Service type<br/>
-          <select name="entry.1662257326">
+          <Select name="entry.1662257326">
             <option>Wedding</option>
             <option>Commercial</option>
             <option>Fashion</option>
             <option>Lessons</option>
             <option>Boudoir</option>
             <option>Other</option>
-          </select>
+          </Select>
         </label>
       </p>
 
       <p>
         <label>
           Event date<br/>
-          <input type="date" autocomplete="off" />
+          <Input type="date" />
           <input type="hidden" name="entry.887843388_year" value="2018" />
           <input type="hidden" name="entry.887843388_month" value="3" />
           <input type="hidden" name="entry.887843388_day" value="15" />
@@ -52,27 +55,28 @@ const Contact = () => (
       <p>
         <label>
           Event location<br/>
-          <input type="text" aria-label="Event location" name="entry.577337949" />
+          <Input type="text" aria-label="Event location" name="entry.577337949" />
         </label>
       </p>
 
       <p>
         <label>
           What time will you need to be ready by?<br/>
+          <Input type="time" step="900" />
         </label>
       </p>
 
       <p>
         <label>
           Size of bridal party<br/>
-          <input type="text" name="entry.1752902834" />
+          <Input type="text" name="entry.1752902834" />
         </label>
       </p>
 
       <p>
         <label>
           Please describe what you're looking for<br/>
-          <textarea name="entry.1525442277"></textarea>
+          <Textarea name="entry.1525442277"></Textarea>
         </label>
       </p>
 
