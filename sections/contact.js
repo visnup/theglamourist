56eb798824fd0cfcb170a83c2d21b8709aa0a1c6
Visnu Pitiyanuvath <visnup@gmail.com>
2018-03-13 23:49:15 -0700

    some contact info

--- sections/contact.js
+++ sections/contact.js
@@ -10,7 +10,21 @@ const Contact = (props) => (
         <Form />
       </Column>
       <Column xs={12} sm={6}>
-        Hello
+        Jane Kim<br/>
+        jane@theglamourist.com<br/>
+        (415) 860-5872
+
+        <h2>Locations</h2>
+        <h3>Studio & Appointments</h3>
+        Blu Bungalow<br/>
+        2068 Union St.<br/>
+        San Francisco, CA 94043
+
+        <h3>Mail & Correspondence</h3>
+        1301 Indiana St. Ste. 203<br/>
+        San Francisco, CA 94107<br/>
+
+        Available for travel worldwide.
       </Column>
     </Row>
   </Section>
