c331d2ec4be6b211da0f60ebcae5ac4e78544186
Visnu Pitiyanuvath <visnup@gmail.com>
2018-03-15 01:49:38 -0700

    mobile width

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,7 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           <title>The Glamourist - Jane Kim - Makeup Artist (San Francisco)</title>
+          <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no" />
           <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond:400i|Montserrat" rel="stylesheet" />
           <link href="https://unpkg.com/normalize.css@8.0.0/normalize.css" rel="stylesheet" />
           {this.props.styleTags}
