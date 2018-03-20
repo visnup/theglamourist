ed05d3d74a8db49a2142c5a3edcd9202851de582
Visnu Pitiyanuvath <visnup@gmail.com>
2018-03-19 23:23:47 -0700

    less awkward header

--- components/section.js
+++ components/section.js
@@ -1,11 +1,10 @@
 import styled from 'styled-components'
 
 export default styled.section`
-  margin: 8em 0;
-  padding: 2em;
+  margin: 6em 0;
+  padding: .5em 2em 2em;
 
   > h1 {
-    margin-top: -1.92em;
     text-align: center;
     border-bottom: solid 1px black;
   }
